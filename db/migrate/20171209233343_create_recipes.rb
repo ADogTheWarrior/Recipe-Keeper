c986d4a1847ba275afc4413c32e18f0cb761ae18
Adam Fudge <coolfudge@gmail.com>
2017-12-10 00:19:56 +0000

    added class variables for functionality

--- db/migrate/20171209233343_create_recipes.rb
+++ db/migrate/20171209233343_create_recipes.rb
@@ -1,7 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :content
+      t.integer :user_id
       t.timestamps null: false
     end
   end
