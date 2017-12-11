2adfb7bb877c7acacd38e25de765c87bf78a901a
Adam Fudge <coolfudge@gmail.com>
2017-12-11 18:19:10 +0000

    worked on creating users and redirecting them to recipes once logged in

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,29 @@
 class SessionController < ApplicationController
-  get "/login" do
-    erb :login
+  get '/login' do
+    if logged_in?
+      redirect "/recipes"
+    else
+      erb :login
+    end
   end
 
   post "/login" do
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/recipes"
+    else
+      redirect "/login"
+    end
   end
 
   get "/logout" do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
   end
 end
