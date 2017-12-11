967d851f4353c2339f7d0fb52f7becad228f0cad
Adam Fudge <coolfudge@gmail.com>
2017-12-11 18:21:50 +0000

    finished get login route

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,22 +8,8 @@ class SessionController < ApplicationController
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/recipes"
-    else
-      redirect "/login"
-    end
   end
 
   get "/logout" do
-    if logged_in?
-      session.clear
-      redirect "/login"
-    else
-      redirect "/"
-    end
   end
 end
