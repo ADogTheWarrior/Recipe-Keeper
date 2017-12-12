36e165c2cbc1e3456272f3e2cf8fe12af7c4ba27
Adam Fudge <coolfudge@gmail.com>
2017-12-12 21:30:28 +0000

    refactored some code

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username])
+    user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
