072e38f594933983db69b6c96d87f57026baedb1
Adam Fudge <coolfudge@gmail.com>
2017-12-12 21:24:52 +0000

    fixed bug with saving an existing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,13 @@ class UsersController < ApplicationController
   # POST: /users
   post "/users" do
     if params[:username] != "" && params[:email] != "" && params[:password] != ""
-      user = User.find_or_create_by(username: params[:username], email: params[:email], password: params[:password])
+
+      if User.find_by(username: params[:username], email: params[:email]) != nil
+        redirect "/login"
+      end
+
+      user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
       if user.save
         session[:user_id] = user.id
         redirect "/recipes"
