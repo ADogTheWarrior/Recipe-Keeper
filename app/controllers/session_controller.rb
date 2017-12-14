7a12f7b09324f4f9f4589fd1532440ebe7675843
Adam Fudge <coolfudge@gmail.com>
2017-12-14 22:23:20 +0000

    logout now directs to welcome page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -21,7 +21,7 @@ class SessionController < ApplicationController
   get "/logout" do
     if logged_in?
       session.clear
-      redirect "/login"
+      erb :welcome
     else
       redirect "/"
     end
