4b6807f9cebe4dbea2bb4b71ee26b2534fe4a47b
Adam Fudge <coolfudge@gmail.com>
2017-12-30 18:55:59 +0000

    fixed the bug with the recipe content attribute, had to reset the database

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20171209233343) do
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
+    t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
